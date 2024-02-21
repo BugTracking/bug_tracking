970836bf9a3c5a5a1d5d6b3bb772ea50c7e7421e
Mohammed Adel <57016916+MoAdelDev@users.noreply.github.com>
2024-02-20 19:41:30 -0800

    Added to router

---
+++ lib/features/get_started/ui/screens/get_started_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GetStartedScreen extends StatelessWidget {
+  const GetStartedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
