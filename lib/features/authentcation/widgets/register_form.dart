339b84642a0e03d1f34664df180af16dd9cc85f8
Mohammed Adel <57016916+MoAdelDev@users.noreply.github.com>
2024-02-26 08:45:20 -0800

    Edited style of button

--- lib/features/authentcation/widgets/register_form.dart
+++ lib/features/authentcation/widgets/register_form.dart
@@ -4,7 +4,6 @@ import 'package:bug_tracking/core/style/app_texts.dart';
 import 'package:bug_tracking/core/helpers/spacing.dart';
 import 'package:bug_tracking/core/widgets/custom_textfield.dart';
 
-
 class RegisterForm extends StatefulWidget {
   const RegisterForm({Key? key}) : super(key: key);
 
@@ -19,107 +18,108 @@ class _RegisterFormState extends State<RegisterForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CustomTextField(
-                hintText: 'Name',
-                prefixIcon:
-                    const Icon(Icons.person, color: AppColor.greyish),
-                hintStyle: AppTexts.text16GreyNunitoSansRegular,
-                errorMsg: 'Field cannot be empty',
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.done,
-              ),
-              verticalSpace(16.0),
-              CustomTextField(
-                hintText: 'User Name',
-                prefixIcon:
-                    const Icon(Icons.person, color: AppColor.greyish),
-                hintStyle: AppTexts.text16GreyNunitoSansRegular,
-                errorMsg: 'Field cannot be empty',
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.done,
-              ),
-              verticalSpace(16.0),
-              CustomTextField(
-                hintText: 'Email',
-                prefixIcon: const Icon(
-                  Icons.email,
-                  color: AppColor.greyish,
-                ),
-                hintStyle: AppTexts.text16GreyNunitoSansRegular,
-                errorMsg: 'Field cannot be empty',
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.done,
-              ),
-
-              verticalSpace(16.0),
-
-              CustomTextField(
-                hintText: 'Phone',
-                prefixIcon: const Icon(
-                  Icons.phone_android,
-                  color: AppColor.greyish,
-                ),
-                hintStyle: AppTexts.text16GreyNunitoSansRegular,
-                errorMsg: 'Field cannot be empty',
-                keyboardType: TextInputType.number,
-                textInputAction: TextInputAction.done,
-              ),
-
-              verticalSpace(16.0),
-
-              CustomTextField(
-                obscureText: _passwordVisible,
-                hintText: 'Password',
-                hintStyle: AppTexts.text16GreyNunitoSansRegular,
-                prefixIcon: const Icon(
-                  Icons.lock,
-                  color: AppColor.greyish,
-                ),
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _passwordVisible = !_passwordVisible;
-                    });
-                  },
-                  icon: Icon(
-                      _passwordVisible ? Icons.visibility_off : Icons.visibility,
-                      color: AppColor.greyish),
-                ),
-                errorMsg: 'Field cannot be empty',
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.done,
-              ),
-
-              verticalSpace(16.0),
-              CustomTextField(
-                obscureText: _confirmPasswordVisible,
-                hintText: 'Confirm Password',
-                hintStyle: AppTexts.text16GreyNunitoSansRegular,
-                prefixIcon: const Icon(Icons.lock, color: AppColor.greyish),
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _confirmPasswordVisible = !_confirmPasswordVisible;
-                    });
-                  },
-                  icon: Icon(
-                      _confirmPasswordVisible ? Icons.visibility_off : Icons.visibility,
-                      color: AppColor.greyish),
-                ),
-                errorMsg: 'Field cannot be empty',
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.done,
-              ),
-
-
-              // HaveAccount(),
-            ],
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CustomTextField(
+            hintText: 'Name',
+            prefixIcon: const Icon(Icons.person, color: AppColor.greyish),
+            hintStyle: AppTexts.text16GreyNunitoSansRegular,
+            errorMsg: 'Field cannot be empty',
+            keyboardType: TextInputType.text,
+            textInputAction: TextInputAction.done,
           ),
+          verticalSpace(16.0),
+          CustomTextField(
+            hintText: 'User Name',
+            prefixIcon: const Icon(Icons.person, color: AppColor.greyish),
+            hintStyle: AppTexts.text16GreyNunitoSansRegular,
+            errorMsg: 'Field cannot be empty',
+            keyboardType: TextInputType.text,
+            textInputAction: TextInputAction.done,
+          ),
+          verticalSpace(16.0),
+          CustomTextField(
+            hintText: 'Email',
+            prefixIcon: const Icon(
+              Icons.email,
+              color: AppColor.greyish,
+            ),
+            hintStyle: AppTexts.text16GreyNunitoSansRegular,
+            errorMsg: 'Field cannot be empty',
+            keyboardType: TextInputType.text,
+            textInputAction: TextInputAction.done,
+          ),
+
+          verticalSpace(16.0),
+
+          CustomTextField(
+            hintText: 'Phone',
+            prefixIcon: const Icon(
+              Icons.phone_android,
+              color: AppColor.greyish,
+            ),
+            hintStyle: AppTexts.text16GreyNunitoSansRegular,
+            errorMsg: 'Field cannot be empty',
+            keyboardType: TextInputType.number,
+            textInputAction: TextInputAction.done,
+          ),
+
+          verticalSpace(16.0),
+
+          CustomTextField(
+            obscureText: _passwordVisible,
+            hintText: 'Password',
+            hintStyle: AppTexts.text16GreyNunitoSansRegular,
+            prefixIcon: const Icon(
+              Icons.lock,
+              color: AppColor.greyish,
+            ),
+            suffixIcon: IconButton(
+              onPressed: () {
+                setState(() {
+                  _passwordVisible = !_passwordVisible;
+                });
+              },
+              icon: Icon(
+                _passwordVisible ? Icons.visibility : Icons.visibility_off,
+                color: AppColor.greyish,
+              ),
+            ),
+            errorMsg: 'Field cannot be empty',
+            keyboardType: TextInputType.text,
+            textInputAction: TextInputAction.done,
+          ),
+
+          verticalSpace(16.0),
+          CustomTextField(
+            obscureText: _confirmPasswordVisible,
+            hintText: 'Confirm Password',
+            hintStyle: AppTexts.text16GreyNunitoSansRegular,
+            prefixIcon: const Icon(Icons.lock, color: AppColor.greyish),
+            suffixIcon: IconButton(
+              onPressed: () {
+                setState(() {
+                  _confirmPasswordVisible = !_confirmPasswordVisible;
+                });
+              },
+              icon: Icon(
+                _confirmPasswordVisible
+                    ? Icons.visibility
+                    : Icons.visibility_off,
+                color: AppColor.greyish,
+              ),
+            ),
+            errorMsg: 'Field cannot be empty',
+            keyboardType: TextInputType.text,
+            textInputAction: TextInputAction.done,
+          ),
+
+          // HaveAccount(),
+        ],
+      ),
     );
   }
 }
--- lib/features/get_started/ui/widgets/get_started_buttons.dart
+++ lib/features/get_started/ui/widgets/get_started_buttons.dart
@@ -1,7 +1,7 @@
 import 'package:bug_tracking/core/helpers/spacing.dart';
-import 'package:bug_tracking/core/style/app_color.dart';
 import 'package:bug_tracking/core/widgets/custom_button.dart';
 import 'package:bug_tracking/core/router/routes.dart';
+import 'package:bug_tracking/core/widgets/custom_outlined_button.dart';
 import 'package:flutter/material.dart';
 
 class GetStartedButtons extends StatelessWidget {
@@ -14,18 +14,15 @@ class GetStartedButtons extends StatelessWidget {
         CustomButton(
           onPressed: () {
             Navigator.pushNamed(context, Routes.register);
-
           },
           text: 'Register',
         ),
         verticalSpace(10.0),
-        CustomButton(
+        CustomOutlinedButton(
           onPressed: () {
             Navigator.pushNamed(context, Routes.login);
           },
           text: 'Login',
-          color: Colors.white,
-          textColor: AppColor.bluish,
         ),
         verticalSpace(40.0),
       ],
