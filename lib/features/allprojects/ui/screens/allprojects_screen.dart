98fed564b240ecb4ba00547a69e878562cf1ec5e
Gowhara <yasmiin.21eid@gmail.com>
2024-02-28 14:45:16 +0200

    adding custom navigation bar to the core styles

--- lib/features/allprojects/ui/screens/allprojects_screen.dart
+++ lib/features/allprojects/ui/screens/allprojects_screen.dart
@@ -2,6 +2,7 @@ import 'package:bug_tracking/core/style/app_color.dart';
 import 'package:bug_tracking/features/allprojects/ui/widgets/project_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:bug_tracking/core/widgets/custom_buttom_Navigation.dart';
 class AllProjectsScreen extends StatelessWidget {
   const AllProjectsScreen({Key? key}) : super(key: key);
 
@@ -28,6 +29,15 @@ class AllProjectsScreen extends StatelessWidget {
         ),
         child: const ProjectList(),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: const CircleBorder(),
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation:
+      FloatingActionButtonLocation.miniCenterDocked,
+      bottomNavigationBar: const CustomButtomNavigation(),
+
     );
   }
 }
