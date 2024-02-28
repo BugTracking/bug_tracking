9ef38d8f0531a32c4bdaf9c74521e67c437db6d0
Gowhara <yasmiin.21eid@gmail.com>
2024-02-28 11:53:53 +0200

    intializing allprojects screen

---
+++ lib/features/allprojects/ui/screens/allprojects_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class AllProjectsScreen extends StatelessWidget {
+  const AllProjectsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Text('projects screen'),
+    );
+  }
+}
