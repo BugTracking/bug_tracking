94b1ca00e0802ad6c8415ea6db074462825aa6a6
Gowhara <yasmiin.21eid@gmail.com>
2024-02-28 11:56:13 +0200

    adding allprojects route

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -11,6 +11,7 @@ import 'package:bug_tracking/features/project_details/ui/screens/project_details
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bug_tracking/features/edit_profile/ui/screens/edit_profile_screen.dart';
+import 'package:bug_tracking/features/allprojects/ui/screens/allprojects_screen.dart';
 
 class AppRouter {
   Route? onGeneratedRoute(RouteSettings settings) {
@@ -53,6 +54,11 @@ class AppRouter {
       case Routes.editProfile:
         return MaterialPageRoute(
           builder: (context) => const EditProfileScreen(),
+        );
+
+      case Routes.allProjects:
+        return MaterialPageRoute(
+          builder: (context) => const AllProjectsScreen(),
         );
       default:
         return null;
