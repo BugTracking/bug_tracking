f44557caefe630e521bb3d06387a9ff257084aea
Gowhara <yasmiin.21eid@gmail.com>
2024-02-29 12:12:43 +0200

    intializing notfication screen

---
+++ lib/features/notfications/ui/screens/notfications.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class NotficationsScreen extends StatelessWidget {
+  const NotficationsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
