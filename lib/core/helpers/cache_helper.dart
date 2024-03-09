846dba841f164ff7d00be6468926193410a9ef38
Mohammed Adel <57016916+MoAdelDev@users.noreply.github.com>
2024-03-08 21:18:48 -0800

    Update token method in cache helper

--- lib/core/helpers/cache_helper.dart
+++ lib/core/helpers/cache_helper.dart
@@ -5,16 +5,15 @@ class CacheHelper {
 
   static Future init() async {
     sharedPreferences = await SharedPreferences.getInstance();
-    if(!sharedPreferences.containsKey('token')){
+    if (!sharedPreferences.containsKey('token')) {
       await setString(key: 'token', value: '');
     }
   }
 
-  static Future<bool> setString({required String key, required String value}) async {
+  static Future<bool> setString(
+      {required String key, required String value}) async {
     return sharedPreferences.setString(key, value);
   }
 
-  static String? getString({required String key}) {
-    return sharedPreferences.getString(key);
-  }
+  static get token => sharedPreferences.getString('token');
 }
--- lib/features/on_boarding/ui/widgets/skip_text_button.dart
+++ lib/features/on_boarding/ui/widgets/skip_text_button.dart
@@ -2,7 +2,6 @@ import 'package:bug_tracking/core/helpers/extensions.dart';
 import 'package:bug_tracking/core/router/routes.dart';
 import 'package:bug_tracking/core/style/app_texts.dart';
 import 'package:flutter/material.dart';
-import 'package:bug_tracking/core/helpers/cache_helper.dart';
 
 class SkipTextButton extends StatelessWidget {
   const SkipTextButton({
@@ -11,14 +10,7 @@ class SkipTextButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () => {
-        if(CacheHelper.getString(key: 'token')==null){
-          context.push(Routes.getStarted)
-        }
-        else{
-          context.push(Routes.home)
-        }
-      },
+      onPressed: () => context.push(Routes.getStarted),
       style: TextButton.styleFrom(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10.0),
