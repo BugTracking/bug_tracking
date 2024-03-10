e16ab418ecb824912a3dfe1994c0f185682c343d
Mohammed Adel <57016916+MoAdelDev@users.noreply.github.com>
2024-03-09 23:56:09 -0800

    Update notfications_list.dart

--- lib/features/notfications/ui/widgets/notfications_list.dart
+++ lib/features/notfications/ui/widgets/notfications_list.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:bug_tracking/core/helpers/spacing.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bug_tracking/features/notfications/data/models/notfication_response_model.dart';
+
 class NotficationList extends StatelessWidget {
   const NotficationList({Key? key}) : super(key: key);
 
@@ -18,24 +19,24 @@ class NotficationList extends StatelessWidget {
         if (cubit.notfications == null) {
           return const CustomShimmerList();
         }
-        final List<NotificationData> notfication = context.read<NotficationCubit>().notfications?? [];
-        if (notfication.isEmpty) {
-          return Text(
-            'There is no Notfications',
-            style: AppTexts.text16OnBackgroundNunitoSansBold,
+        final List<NotificationData> notfications =
+            context.read<NotficationCubit>().notfications ?? [];
+        if (notfications.isEmpty) {
+          return Center(
+            child: Text(
+              'There is no Notfications',
+              style: AppTexts.text16OnBackgroundNunitoSansBold,
+            ),
           );
         }
-        return SafeArea(
-          child: ListView.separated(
-            itemBuilder: (context, index) {
-              NotificationData notfi= notfication[index];
-              return  NotificationsTile(notfication: notfi);
-            },
-            clipBehavior: Clip.none,
-            separatorBuilder: (context, index) => verticalSpace(20.0),
-            shrinkWrap: true,
-            itemCount: notfication.isNotEmpty? notfication.length :0,
-          ),
+        return ListView.separated(
+          itemBuilder: (context, index) {
+            NotificationData notfi = notfications[index];
+            return NotificationsTile(notfication: notfi);
+          },
+          separatorBuilder: (context, index) => verticalSpace(20.0),
+          shrinkWrap: true,
+          itemCount: notfications.isNotEmpty ? notfications.length : 0,
         );
       },
     );
