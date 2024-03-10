5641a68880360749d73de03ead79982523cf3938
Mohammed Adel <57016916+MoAdelDev@users.noreply.github.com>
2024-03-09 23:54:03 -0800

    Update notfication_cubit.dart

--- lib/features/notfications/logic/cubit/notfication_cubit.dart
+++ lib/features/notfications/logic/cubit/notfication_cubit.dart
@@ -6,13 +6,11 @@ import 'package:bug_tracking/features/notfications/data/models/notfication_respo
 import 'package:bug_tracking/features/notfications/data/repos/notfication_repo.dart';
 import 'package:bug_tracking/features/notfications/logic/cubit/notfication_state.dart';
 
-
 class NotficationCubit extends Cubit<NotficationState> {
   final NotficationRepo _notficationRepo;
 
   NotficationCubit(this._notficationRepo)
       : super(const NotficationState.initial());
-
 
   List<NotificationData>? notfications;
 
@@ -21,7 +19,7 @@ class NotficationCubit extends Cubit<NotficationState> {
         CacheHelper.token, CacheHelper.userId);
     response.when(
       success: (data) {
-        notfications = data.data ?? [];
+        notfications = data.data;
         emit(GetNotficationSuccess(notfications!));
       },
       failure: (error) {
@@ -30,4 +28,3 @@ class NotficationCubit extends Cubit<NotficationState> {
     );
   }
 }
-
