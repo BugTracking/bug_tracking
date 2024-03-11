25ae178fa52daaf96277515ad5b54e3c6f1e5d6d
Mohammed Adel <57016916+MoAdelDev@users.noreply.github.com>
2024-03-11 15:48:20 -0700

    Bug filter done successfully

---
+++ lib/features/allbugs/logic/cubit/bugs_state.freezed.dart
@@ -0,0 +1,404 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'bugs_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$BugsState {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() selectItem,
+    required TResult Function() filterBugs,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function()? selectItem,
+    TResult? Function()? filterBugs,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? selectItem,
+    TResult Function()? filterBugs,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(SelectItem value) selectItem,
+    required TResult Function(FilterBugs value) filterBugs,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(SelectItem value)? selectItem,
+    TResult? Function(FilterBugs value)? filterBugs,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(SelectItem value)? selectItem,
+    TResult Function(FilterBugs value)? filterBugs,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $BugsStateCopyWith<$Res> {
+  factory $BugsStateCopyWith(BugsState value, $Res Function(BugsState) then) =
+      _$BugsStateCopyWithImpl<$Res, BugsState>;
+}
+
+/// @nodoc
+class _$BugsStateCopyWithImpl<$Res, $Val extends BugsState>
+    implements $BugsStateCopyWith<$Res> {
+  _$BugsStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+}
+
+/// @nodoc
+abstract class _$$InitialImplCopyWith<$Res> {
+  factory _$$InitialImplCopyWith(
+          _$InitialImpl value, $Res Function(_$InitialImpl) then) =
+      __$$InitialImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$InitialImplCopyWithImpl<$Res>
+    extends _$BugsStateCopyWithImpl<$Res, _$InitialImpl>
+    implements _$$InitialImplCopyWith<$Res> {
+  __$$InitialImplCopyWithImpl(
+      _$InitialImpl _value, $Res Function(_$InitialImpl) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$InitialImpl implements _Initial {
+  const _$InitialImpl();
+
+  @override
+  String toString() {
+    return 'BugsState.initial()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$InitialImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() selectItem,
+    required TResult Function() filterBugs,
+  }) {
+    return initial();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function()? selectItem,
+    TResult? Function()? filterBugs,
+  }) {
+    return initial?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? selectItem,
+    TResult Function()? filterBugs,
+    required TResult orElse(),
+  }) {
+    if (initial != null) {
+      return initial();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(SelectItem value) selectItem,
+    required TResult Function(FilterBugs value) filterBugs,
+  }) {
+    return initial(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(SelectItem value)? selectItem,
+    TResult? Function(FilterBugs value)? filterBugs,
+  }) {
+    return initial?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(SelectItem value)? selectItem,
+    TResult Function(FilterBugs value)? filterBugs,
+    required TResult orElse(),
+  }) {
+    if (initial != null) {
+      return initial(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Initial implements BugsState {
+  const factory _Initial() = _$InitialImpl;
+}
+
+/// @nodoc
+abstract class _$$SelectItemImplCopyWith<$Res> {
+  factory _$$SelectItemImplCopyWith(
+          _$SelectItemImpl value, $Res Function(_$SelectItemImpl) then) =
+      __$$SelectItemImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$SelectItemImplCopyWithImpl<$Res>
+    extends _$BugsStateCopyWithImpl<$Res, _$SelectItemImpl>
+    implements _$$SelectItemImplCopyWith<$Res> {
+  __$$SelectItemImplCopyWithImpl(
+      _$SelectItemImpl _value, $Res Function(_$SelectItemImpl) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$SelectItemImpl implements SelectItem {
+  const _$SelectItemImpl();
+
+  @override
+  String toString() {
+    return 'BugsState.selectItem()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$SelectItemImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() selectItem,
+    required TResult Function() filterBugs,
+  }) {
+    return selectItem();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function()? selectItem,
+    TResult? Function()? filterBugs,
+  }) {
+    return selectItem?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? selectItem,
+    TResult Function()? filterBugs,
+    required TResult orElse(),
+  }) {
+    if (selectItem != null) {
+      return selectItem();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(SelectItem value) selectItem,
+    required TResult Function(FilterBugs value) filterBugs,
+  }) {
+    return selectItem(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(SelectItem value)? selectItem,
+    TResult? Function(FilterBugs value)? filterBugs,
+  }) {
+    return selectItem?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(SelectItem value)? selectItem,
+    TResult Function(FilterBugs value)? filterBugs,
+    required TResult orElse(),
+  }) {
+    if (selectItem != null) {
+      return selectItem(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class SelectItem implements BugsState {
+  const factory SelectItem() = _$SelectItemImpl;
+}
+
+/// @nodoc
+abstract class _$$FilterBugsImplCopyWith<$Res> {
+  factory _$$FilterBugsImplCopyWith(
+          _$FilterBugsImpl value, $Res Function(_$FilterBugsImpl) then) =
+      __$$FilterBugsImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$FilterBugsImplCopyWithImpl<$Res>
+    extends _$BugsStateCopyWithImpl<$Res, _$FilterBugsImpl>
+    implements _$$FilterBugsImplCopyWith<$Res> {
+  __$$FilterBugsImplCopyWithImpl(
+      _$FilterBugsImpl _value, $Res Function(_$FilterBugsImpl) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$FilterBugsImpl implements FilterBugs {
+  const _$FilterBugsImpl();
+
+  @override
+  String toString() {
+    return 'BugsState.filterBugs()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$FilterBugsImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() selectItem,
+    required TResult Function() filterBugs,
+  }) {
+    return filterBugs();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function()? selectItem,
+    TResult? Function()? filterBugs,
+  }) {
+    return filterBugs?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? selectItem,
+    TResult Function()? filterBugs,
+    required TResult orElse(),
+  }) {
+    if (filterBugs != null) {
+      return filterBugs();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(SelectItem value) selectItem,
+    required TResult Function(FilterBugs value) filterBugs,
+  }) {
+    return filterBugs(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(SelectItem value)? selectItem,
+    TResult? Function(FilterBugs value)? filterBugs,
+  }) {
+    return filterBugs?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(SelectItem value)? selectItem,
+    TResult Function(FilterBugs value)? filterBugs,
+    required TResult orElse(),
+  }) {
+    if (filterBugs != null) {
+      return filterBugs(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class FilterBugs implements BugsState {
+  const factory FilterBugs() = _$FilterBugsImpl;
+}
--- lib/features/filter/screen/bug_filter_screen.dart
+++ lib/features/filter/screen/bug_filter_screen.dart
@@ -1,115 +1,91 @@
+import 'package:bug_tracking/core/helpers/constants.dart';
 import 'package:bug_tracking/core/helpers/spacing.dart';
 import 'package:bug_tracking/core/style/app_texts.dart';
 import 'package:bug_tracking/core/widgets/custom_divider.dart';
-import 'package:bug_tracking/features/filter/widgets/bug_date.dart';
+import 'package:bug_tracking/features/allbugs/logic/cubit/bugs_cubit.dart';
+import 'package:bug_tracking/features/allbugs/logic/cubit/bugs_state.dart';
 import 'package:bug_tracking/features/filter/widgets/bug_filter_submit.dart';
 import 'package:bug_tracking/features/filter/widgets/filter_box.dart';
 import 'package:bug_tracking/features/filter/widgets/filter_container.dart';
+import 'package:bug_tracking/features/home/data/models/bugs_response_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class BugFilterScreen extends StatefulWidget {
-  const BugFilterScreen({Key? key}) : super(key: key);
-
-  @override
-  State<BugFilterScreen> createState() => _BugFilterScreenState();
-}
-
-class _BugFilterScreenState extends State<BugFilterScreen> {
-  List<String> projectNames = [
-    'All',
-    'Saveto' 'BugTracker',
-    'Salla',
-    'ToDo',
-    'Bug'
-  ];
-  int projectNameSelected = 0;
-  List<String> bugStatus = ['To DO', 'In progress', 'Done'];
-  int bugStatusSelected = 0;
+class BugFilterScreen extends StatelessWidget {
+  final List<BugModel> bugs;
+  const BugFilterScreen({super.key, required this.bugs});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 20.w,
-          vertical: 50.0.h,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const FilterBox(),
-            verticalSpace(20.0),
-            Column(
+    return BlocBuilder<BugsCubit, BugsState>(
+      builder: (context, state) {
+        BugsCubit cubit = context.read<BugsCubit>();
+        return Scaffold(
+          body: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.w,
+              vertical: 50.0.h,
+            ),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
               children: [
-                Text(
-                  "Project Name ",
-                  style: AppTexts.text14PrimaryNunitoSansBold,
-                ),
-                Wrap(
-                  runSpacing: 8.0,
-                  children: List.generate(
-                    projectNames.length,
-                    (index) => GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          projectNameSelected = index;
-                        });
-                      },
-                      child: FilterContainer(
-                        text: projectNames[index],
-                        onTap: () {},
-                        isSelected: projectNameSelected == index,
-                      ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        const FilterBox(),
+                        verticalSpace(20.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Bug Statuses ",
+                              style: AppTexts.text14PrimaryNunitoSansBold,
+                            ),
+                            verticalSpace(10.0),
+                            Wrap(
+                              runSpacing: 8.0,
+                              children: [
+                                FilterContainer(
+                                  text: 'All Statuses',
+                                  isSelected:
+                                      cubit.bugStatusSelected == 'All Statuses',
+                                  onTap: () =>
+                                      cubit.selectBugStatus('All Statuses'),
+                                ),
+                                ...[
+                                  inProgress,
+                                  done,
+                                  toDo,
+                                ].map(
+                                  (e) => FilterContainer(
+                                    text: e,
+                                    isSelected: cubit.bugStatusSelected == e,
+                                    onTap: () => cubit.selectBugStatus(e),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            verticalSpace(15.0),
+                            const CustomDivider(),
+                          ],
+                        ),
+                        verticalSpace(30.0),
+                      ],
                     ),
                   ),
                 ),
-                verticalSpace(15.0),
-                const CustomDivider(),
-              ],
-            ),
-            verticalSpace(15.0),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Dug Status ",
-                    style: AppTexts.text14PrimaryNunitoSansBold),
                 verticalSpace(10.0),
-                Wrap(
-                  runSpacing: 6.0,
-                  children: List.generate(
-                    bugStatus.length,
-                    (index) => GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          bugStatusSelected = index;
-                        });
-                      },
-                      child: FilterContainer(
-                        text: projectNames[index],
-                        isSelected: bugStatusSelected == index,
-                        onTap: () {},
-                      ),
-                    ),
-                  ),
+                BugFilterSubmit(
+                  bugs: bugs,
                 ),
-                verticalSpace(20.0),
-                const CustomDivider(),
-                const DugDate(
-                  dateName: "Created date ",
-                  date: '13/2/2024',
-                ),
-                const DugDate(date: '', dateName: "Last Updated "),
               ],
             ),
-            const Spacer(),
-            const BugFilterSubmit(),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
